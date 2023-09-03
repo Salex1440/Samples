159d2a9179838605a54306aa32791b14fe86df1b
salex <ShutovAlexandr@yandex.ru>
2023-09-03 22:39:16 +0300

    Added hyperlinks to the Person response

--- HATEOAS/src/main/java/com/salex/hateoasexample/service/PersonService.java
+++ HATEOAS/src/main/java/com/salex/hateoasexample/service/PersonService.java
@@ -1,13 +1,18 @@
 package com.salex.hateoasexample.service;
 
+import com.salex.hateoasexample.controller.PersonController;
 import com.salex.hateoasexample.dto.PersonDto;
 import com.salex.hateoasexample.mapper.PersonMapper;
 import com.salex.hateoasexample.model.Person;
 import com.salex.hateoasexample.repo.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Service
 public class PersonService {
@@ -18,8 +23,17 @@ public class PersonService {
     private PersonMapper mapper;
 
     public Person createPerson(PersonDto personDto) {
-        Person person = mapper.fromDto(personDto);
-        return repository.createPerson(person);
+        Person person = repository.createPerson(mapper.fromDto(personDto));
+        Link selfRel = linkTo(PersonController.class).slash(person.getName()).withSelfRel();
+        person.add(selfRel);
+        Link all = linkTo(methodOn(PersonController.class).getPersons()).withRel("all");
+        person.add(all);
+        Link update = linkTo(methodOn(PersonController.class).updatePerson(null))
+            .withRel("post");
+        person.add(update);
+        Link delete = linkTo(methodOn(PersonController.class).deletePerson(person.getName())).withRel("delete");
+        person.add(delete);
+        return person;
     }
 
     public Person updatePerson(PersonDto personDto) {
